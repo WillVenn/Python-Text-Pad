0f8a9d0f1cd0b12cb97477220e202d1437cd0d1d
Will Venn <infohack3@gmail.com>
2014-12-08 21:03:20 +0000

    Added comments for MainApp code

--- TextPad.py
+++ TextPad.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3.4
 
 from tkinter import * #Imports standard tkinter modules
-
+#------------------------------------------------------
+#Window
 root = Tk() #Creates root window
 root.title("Text Pad - Will Venn") #Sets the name/title of said window
+##root.geometry("15x55")
 
-TextArea = Text(root, height=10, width=50) #Creates a text zone or area for the user to write stuff in
+#------------------------------------------------------
+#TextArea
+TextArea = Text(root, height=40, width=90) #Creates a text zone or area for the user to write stuff in
 TextArea.pack(side=LEFT, fill=Y) #Says which side it is on and to fill on the Y axis
-
+#------------------------------------------------------
+#Scroll bar
 Scroll = Scrollbar(root) #Creates a scroll bar on the 'root' window
 Scroll.pack(side=RIGHT, fill=Y)
 Scroll.config(command=TextArea.yview)
 TextArea.config(yscrollcommand=Scroll.set)
+#------------------------------------------------------
 
-
+#window loop
 root.mainloop()
