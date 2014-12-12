6cb85511f8819aff99bd4f061d4ea1fe56f38997
Will Venn <infohack3@gmail.com>
2014-12-12 17:18:20 +0000

    Added keyboard shortcut support

--- TextPad.py
+++ TextPad.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3.4
 
 from tkinter import * #Imports standard tkinter modules
-import tkinter.messagebox
-import tkinter.filedialog
+import tkinter.messagebox #Imports the messagebox function opens a dialog box
+import tkinter.filedialog #Same as the other thing above but for opening files
+import sys #I will be using this for accessing system files and creating keyboard shortcuts
 #------------------------------------------------------
 #Window
 root = Tk() #Creates root window
@@ -12,8 +13,20 @@ root.title("Text Pad - Will Venn") #Sets the name/title of said window
 
 #------------------------------------------------------
 #Functions:
+
+def quit(self):
+	root.quit
+
+
 def about():
-	tkinter.messagebox.showwarning("About", "Sorry this Function is not available yet :/")
+	##tkinter.messagebox.showwarning("About", "Sorry this Function is not available yet :/")
+	about = Tk()
+	about.title("About Python-TextPad")
+	about.geometry("400x250")
+	label_1 = Label(about, text="This is a Python IDE made with Python 3.4 and tkinter").pack()
+	label_2 = Label(about, text="If you would like to know the keyboard shortcuts,").pack()
+	label_3 = Label(about, text="check out the help window").pack()
+	about.mainloop()
 
 def colourChange():
 	tkinter.messagebox.showwarning("Colour change", "Sorry this Function is not available yet :/")	
@@ -21,7 +34,7 @@ def colourChange():
 def sizeChange():
 	tkinter.messagebox.showwarning("Size change", "Sorry this Function is not available yet :/")
 
-def save():
+def save(self):
 	f = tkinter.filedialog.asksaveasfile(mode='w', defaultextension=".txt")
 	if f is None:
 		return
@@ -29,8 +42,8 @@ def save():
 	f.write(text2save)
 	f.close()
 
-def load_file():
-	openfile = tkinter.filedialog.askopenfilename(filetypes = ( ("Python files", "*.py"), ("All files", "*.*") ) )
+def load_file(self):
+	openfile = tkinter.filedialog.askopenfilename(title="Open a python file...", filetypes = ( ("Python files", "*.py"), ("All files", "*.*") ) )
 	print ("launched load_file")
 	if openfile != None:
 		content = open(openfile).read()
@@ -39,7 +52,7 @@ def load_file():
 		openfile.close()
 
 
-def messagebox():
+def messagebox(self):
 	tkinter.messagebox.showwarning("info", "message")
 
 #------------------------------------------------------
@@ -47,8 +60,13 @@ def messagebox():
 #TextArea text = open(filename).read() TextArea.delete(1.0, END)
 #TextArea.insert(END, text) TextArea.mark_set(INSERT, 1.0)
 
-TextArea = Text(root, height=60, width=120) #Creates a text zone or area for the user to write stuff in
+TextArea = Text(root, height=60, width=160) #Creates a text zone or area for the user to write stuff in
 TextArea.pack(side=LEFT, fill=Y) #Says which side it is on and to fill on the Y axis
+
+TextArea.bind_all("<Control-s>", save)
+TextArea.bind_all("<Control-q>", quit)
+TextArea.bind_all("<Control-o>", load_file)
+
 #------------------------------------------------------ 
 #Scroll bar
 Scroll = Scrollbar(root) #Creates a scroll bar on the 'root' window
