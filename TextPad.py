087e22c21971db753f7d1ccab1adb47e7449fc44
Will Venn <infohack3@gmail.com>
2014-12-14 10:57:01 +0000

    Fixed shortcut bugs except control-q

--- TextPad.py
+++ TextPad.py
@@ -2,7 +2,8 @@
 
 from tkinter import * #Imports standard tkinter modules
 import tkinter.messagebox #Imports the messagebox function opens a dialog box
-import tkinter.filedialog #Same as the other thing above but for opening files
+import tkinter.filedialog #Same as the other thing above but for opening file
+import tkinter.font #Changes the font for the text and the labels
 import sys #I will be using this for accessing system files and creating keyboard shortcuts
 #------------------------------------------------------
 #Window
@@ -10,7 +11,9 @@ root = Tk() #Creates root window
 root.title("Text Pad - Will Venn") #Sets the name/title of said window
 ##root.geometry("15x55")
 
-
+#------------------------------------------------------
+#Custom fonts
+customFont = tkinter.font.Font(family="Helvetica", size=14)
 #------------------------------------------------------
 #Functions:
 
@@ -28,13 +31,24 @@ def about():
 	label_3 = Label(about, text="check out the help window").pack()
 	about.mainloop()
 
+def help():
+	help = Tk()
+	help.title("Help")
+	help.geometry("400x250")
+	label_shortcuts = Label(help, text="keyboard shortcuts:").pack()
+	label_quit = Label(help, text="Control-q quits the IDE").pack()
+	label_save = Label(help, text="Control-s saves the current document").pack()
+	label_open = Label(help, text="Control-o opens an existing text file").pack()
+	help.mainloop()
+
 def colourChange():
 	tkinter.messagebox.showwarning("Colour change", "Sorry this Function is not available yet :/")	
 
 def sizeChange():
 	tkinter.messagebox.showwarning("Size change", "Sorry this Function is not available yet :/")
+	TextArea.config(customFont)
 
-def save(self):
+def save():
 	f = tkinter.filedialog.asksaveasfile(mode='w', defaultextension=".txt")
 	if f is None:
 		return
@@ -42,7 +56,25 @@ def save(self):
 	f.write(text2save)
 	f.close()
 
-def load_file(self):
+def save_shortcut(self):
+	f = tkinter.filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+	if f is None:
+		return
+	text2save = str(TextArea.get(1.0, END))
+	f.write(text2save)
+	f.close()
+
+
+def load_file():
+	openfile = tkinter.filedialog.askopenfilename(title="Open a python file...", filetypes = ( ("Python files", "*.py"), ("All files", "*.*") ) )
+	print ("launched load_file")
+	if openfile != None:
+		content = open(openfile).read()
+		##content = openfile.read()
+		TextArea.insert("end", content)
+		openfile.close()
+
+def load_file_shortcut(self):
 	openfile = tkinter.filedialog.askopenfilename(title="Open a python file...", filetypes = ( ("Python files", "*.py"), ("All files", "*.*") ) )
 	print ("launched load_file")
 	if openfile != None:
@@ -63,9 +95,9 @@ def messagebox(self):
 TextArea = Text(root, height=60, width=160) #Creates a text zone or area for the user to write stuff in
 TextArea.pack(side=LEFT, fill=Y) #Says which side it is on and to fill on the Y axis
 
-TextArea.bind_all("<Control-s>", save)
+TextArea.bind_all("<Control-s>", save_shortcut)
 TextArea.bind_all("<Control-q>", quit)
-TextArea.bind_all("<Control-o>", load_file)
+TextArea.bind_all("<Control-o>", load_file_shortcut)
 
 #------------------------------------------------------ 
 #Scroll bar
@@ -77,6 +109,7 @@ TextArea.config(yscrollcommand=Scroll.set)
 #Toolbar
 menubar = Menu(root) #Creates the (NON-CASCADING) menubar
 menubar.add_command(label="About", command=about) #Adds an about button that launches my about app
+menubar.add_command(label="Help", command=help)
 menubar.add_command(label="Quit", command=root.quit) #Adds a quit button that closes the whole window/running processes
 ##menubar.add_separator()
 
@@ -89,6 +122,8 @@ menubar.add_command(label="|")
 
 menubar.add_command(label="Save...", command=save)
 menubar.add_command(label="Open...", command=load_file)
+
+menubar.add_command(label="|")
 ##menubar.add_command(label="messagebox", command=messagebox)
 
 root.config(menu=menubar) #Configures the window to appear on the menu
